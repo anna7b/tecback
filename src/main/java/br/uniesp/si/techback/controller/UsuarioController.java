15fa188080e2bf76a87fa877896dc1ba85cad287
anna7b <annaluizabritto@gmail.com>
2025-03-20 16:58:22 -0300

    tarefa fujioka

--- src/main/java/br/uniesp/si/techback/controller/UsuarioController.java
+++ src/main/java/br/uniesp/si/techback/controller/UsuarioController.java
@@ -1,9 +1,9 @@
 package br.uniesp.si.techback.controller;
 
 
-import br.uniesp.si.techback.model.Filme;
-import br.uniesp.si.techback.repository.FilmeRepository;
-import br.uniesp.si.techback.service.FilmeService;
+import br.uniesp.si.techback.model.Usuario;
+import br.uniesp.si.techback.repository.UsuarioRepository;
+import br.uniesp.si.techback.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,26 +11,26 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/filmes")
-public class FilmeController {
+@RequestMapping("/usuarios")
+public class UsuarioController {
 
     @Autowired
-    private FilmeService service;
+    private UsuarioService service;
 
     @GetMapping
-    public List<Filme> listar(){
+    public List<Usuario> listar(){
         return service.listar();
 
     }
 
     @PostMapping
-    public Filme incluir(@RequestBody Filme filme){
-        return service.cadastrar(filme);
+    public Usuario incluir(@RequestBody Usuario usuario){
+        return service.cadastrar(usuario);
     }
-@PutMapping
+    @PutMapping
 
-    public Filme alterar(@RequestBody Filme filme){
-        return service.alterar(filme);
+    public Usuario alterar(@RequestBody Usuario usuario){
+        return service.alterar(usuario);
     }
     @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable Integer id){
